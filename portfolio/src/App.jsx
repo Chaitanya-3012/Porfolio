eaa760a288be5ce3ba5962ed729d04d66371f117
Chaitanya Nevse <chaitanyanevse3012@gmail.com>
2025-07-16 14:47:22 +0530

    feat: add ModelViewer component for 3D model rendering with interactive controls. add InfiniteMenu component for showcasing projects in an interactive way.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import Welcome from "./components/Welcome";
+import InfiniteMenu from "./Ui/InfiniteMenu";
 import Nav from "./Ui/Nav";
 import Particles from "./Ui/Particles";
 export default function App() {
@@ -8,10 +9,51 @@ export default function App() {
     { label: "About", href: "#" },
     { label: "Contact", href: "#" },
   ];
+  const Projects = [
+    {
+      image: "/images/Project-1.jpg",
+      link: "https://google.com/",
+      title: "Project 1",
+      description: "This is Project 1, it is pretty cool!",
+    },
+    {
+      image: "/images/Project-2.jpg",
+      link: "https://google.com/",
+      title: "Project 2",
+      description: "This is Project 2, it is pretty cool!",
+    },
+    {
+      image: "/images/Project-3.jpg",
+      link: "https://google.com/",
+      title: "Project 3",
+      description: "This is Project 3, it is pretty cool!",
+    },
+    {
+      image: "/images/Project-4.jpg",
+      link: "https://google.com/",
+      title: "Project 4",
+      description: "This is Project 4, it is pretty cool!",
+    },
+  ];
   return (
     <>
-      <div className="w-full h-full flex items-center justify-center font-poppins text-1xl">
+      {/*Background*/}
+      <div className="fixed w-full h-full min-h-screen">
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={100}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={false}
+          alphaParticles
+          disableRotation={false}
+        />
+      </div>
+      {/*Foreground*/}
+      <div className="relative w-full h-full flex flex-col items-center justify-center z-1 ">
         <Nav
+          className="sticky top-0 left-0 w-full z-10"
           items={items}
           particleCount={15}
           particleDistances={[90, 10]}
@@ -21,20 +63,18 @@ export default function App() {
           timeVariance={300}
           colors={[1, 2, 3, 1, 2, 3, 1, 4]}
         />
+        <div
+          className="flex items-center justify-center w-full"
+          style={{ height: "90dvh" }}
+        >
+          <Welcome />
+          <div style={{ width: "50dvw" }}></div>
+        </div>
       </div>
-      <div className="relative w-full h-full min-h-screen">
-        <Particles
-          className="fixed inset-0 -z-10 w-full h-full"
-          particleColors={["#ffffff", "#ffffff"]}
-          particleCount={400}
-          particleSpread={20}
-          speed={0.1}
-          particleBaseSize={100}
-          moveParticlesOnHover={false}
-          alphaParticles
-          disableRotation={false}
-        />
-        <Welcome />
+      <br/>
+      <br/>
+      <div className="w-full h-full">
+        <InfiniteMenu items={Projects} />
       </div>
     </>
   );
--- portfolio/src/components/Welcome.jsx
+++ portfolio/src/components/Welcome.jsx
@@ -2,11 +2,11 @@ import DecryptedText from "../Ui/DecryptedText";
 import SplitText from "../Ui/SplitText";
 export default function Welcome() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center w-full h-screen">
       <DecryptedText
         text="Chaitanya Nevse"
-        className="text-3xl w-full h-full font-bold font-poppins tracking-wide font-grey-800"
-        encryptedClassName="text-grey-500 text-5xl"
+        className="text-6xl w-full h-full font-bold font-poppins tracking-wide font-grey-800 relative"
+        encryptedClassName="text-grey-500 text-6xl"
         animateOn="view"
         speed={70}
         sequential={true}
